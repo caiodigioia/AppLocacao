bc22822e8e2ba6fa339751e60d832c2e346c283a
Leonardo Costa <leorizzocosta@hotmail.com>
2023-11-28 20:42:36 -0300

    Atualiza ModuloFuncionario.java com implementação do banco de dados e validação

--- ModuloFuncionario.java
+++ ModuloFuncionario.java
@@ -4,46 +4,34 @@ public class ModuloFuncionario {
 
   public static void main(String args[]) {
     Scanner scan = new Scanner(System.in);
-  
-    Cliente[] clientes = new Cliente[10];
-    int numClientes = 0;
-
     String opt;
+
     do {
       mainMenu();
       System.out.print("-> ");
       opt = scan.nextLine();
 
-      if(opt.equals("cliente")) {
-        System.out.print("Nome Completo: ");
-        String nome = scan.nextLine();
+      switch(opt) {
+        case "cliente":
+          try {
+            cadastroCliente(scan);
+          }
+          catch(ClienteException e) {
+            System.err.println("Erro: " + e);
+          }
+          break;
 
-        System.out.print("CPF: ");
-        String cpf = scan.nextLine();
+        case "listar":
+          Database.listarClientes();
+          break;
 
-        System.out.print("Data de Nascimento (YYYY-MM-DD): ");
-        String dataNascimento = scan.nextLine();
+        case "sair":
+          System.out.println("Encerrando sistema.");
+          break;
 
-        System.out.print("Email: ");
-        String email = scan.nextLine();
-
-        System.out.print("Telefone/Celular: ");
-        String celular = scan.nextLine();
-
-        clientes[numClientes] = new Cliente(cpf, nome, dataNascimento, email, celular);
-        numClientes++;
-      }
-      else  if(opt.equals("listar")) {
-        System.out.println("## Clientes ##");
-        for(int i = 0; i < numClientes; i++) {
-          System.out.println(clientes[i]);
-        }
-      }
-      else if(opt.equals("sair")) {
-        System.out.println("Encerrando sistema.");
-      }
-      else {
-        System.err.println("Erro: comando desconhecido!");
+        default:
+          System.err.println("Erro: comando desconhecido!");
+          break;
       }
     } while(!opt.equals("sair"));
   }
@@ -53,5 +41,50 @@ public class ModuloFuncionario {
     System.out.println("<cliente> Cadastrar novo cliente");
     System.out.println("<listar> Exibe os cilentes cadastrados");
     System.out.println("<sair> Encerra o sistema\n");
+  }
+
+  public static void cadastroCliente(Scanner scan) throws ClienteException {
+    System.out.print("Nome Completo: ");
+    String nome = scan.nextLine();
+    if(ClienteException.nomeInvalido(nome)) {
+      throw new ClienteException("Nome invalido.");
+    }
+
+    System.out.print("CPF: ");
+    String cpf = scan.nextLine();
+    if(ClienteException.cpfInvalido(cpf)) {
+      throw new ClienteException("CPF invalido.");
+    }
+    if(ClienteException.clienteExistente(cpf)) {
+      throw new ClienteException("Cliente ja cadastrado.");
+    }
+
+    System.out.print("Data de Nascimento (YYYY-MM-DD): ");
+    String dataNascimento = scan.nextLine();
+    if(ClienteException.dataNascimentoInvalida(dataNascimento)) {
+      throw new ClienteException("Data de nascimento invalida.");
+    }
+    if(ClienteException.menorDeIdade(dataNascimento)) {
+      throw new ClienteException("Cliente menor de idade.");
+    }
+
+    System.out.print("Email: ");
+    String email = scan.nextLine();
+    if(ClienteException.emailInvalido(email)) {
+      throw new ClienteException("Email invalido.");
+    }
+
+    System.out.print("Telefone/Celular (XX XXXX-XXXX): ");
+    String celular = scan.nextLine();
+    if(ClienteException.celularInvalido(celular)) {
+      throw new ClienteException("Telefone/Celular invalido.");
+    }
+
+    if(ClienteException.dadosNaoPreenchidos(nome, cpf, dataNascimento, email, celular)) {
+      throw new ClienteException("Dados obrigatorios nao informados.");
+    }
+
+    Cliente novoCliente = new Cliente(cpf, nome, dataNascimento, email, celular);
+    Database.adicionarCliente(novoCliente);
   }
 }
